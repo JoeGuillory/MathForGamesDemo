093b19b57062cee95d54fcf2f0f0d2e18e582ce7
JoeGuillory <joeguillory15@gmail.com>
2024-10-31 19:31:24 -0500

    Tank movement
    Completed the tank movement so the forward rotates correctly

--- MathForGamesDemo/src/Game/TankBottom.cs
+++ MathForGamesDemo/src/Game/TankBottom.cs
@@ -23,27 +23,29 @@ namespace MathForGamesDemo
         public override void Start()
         {
             base.Start();
+            
             Transform.LocalPosition = new Vector2(Raylib.GetScreenWidth() / 2,Raylib.GetScreenHeight() / 2);
             tankBottom = Raylib.LoadTexture(@"res\largepng\tankBody_blue.png");
-            tankCenter = new Vector2(tankBottom.Width /2, tankBottom.Height/ 2);
+            tankCenter = new Vector2(Transform.GlobalScale.x * TankScale / 2,Transform.GlobalScale.y * TankScale / 2);
             tankImage = new Rectangle(0,0, tankBottom.Width,tankBottom.Height);
            
-            position = Transform.GlobalPosition;
         }
 
         public override void Update(double deltaTime)
         {
             base.Update(deltaTime);
+            
+            position = Transform.GlobalPosition;
             Vector2 offset = new Vector2((Transform.LocalScale.x * TankScale) / 2, (Transform.LocalScale.y * TankScale) / 2);
 
             Movement(deltaTime);
-            tankDestination = new Rectangle(Transform.GlobalPosition, Transform.GlobalScale * TankScale);
+            tankDestination = new Rectangle(Transform.GlobalPosition + offset, Transform.GlobalScale * TankScale);
             
             Raylib.DrawLine(Raylib.GetScreenWidth() / 2,0,Raylib.GetScreenWidth() / 2,Raylib.GetScreenHeight(),Color.Black);
             Raylib.DrawLine(0, Raylib.GetScreenHeight() / 2, Raylib.GetScreenWidth(),Raylib.GetScreenHeight() / 2,Color.Black);
             Raylib.DrawText(tankBottom.Width + " | " + tankBottom.Height, 50, 50, 20, Color.Black);
             Raylib.DrawText(position.ToString(), 50, 100, 20, Color.Black);
-            Raylib.DrawTexturePro(tankBottom, tankImage, tankDestination,tankCenter, Transform.GlobalRotationAngle * (float)(180/Math.PI) + 90 * RotateSpeed, Color.White);
+            Raylib.DrawTexturePro(tankBottom, tankImage, tankDestination,tankCenter, -1 * Transform.GlobalRotationAngle * (float)(180/Math.PI) + 135  * RotateSpeed, Color.White);
             Raylib.DrawLineV(Transform.GlobalPosition + offset, Transform.GlobalPosition + offset + (Transform.Forward * 100), Color.Black);
            
         }
@@ -58,10 +60,10 @@ namespace MathForGamesDemo
                 Transform.Translate(Transform.Forward * Speed * (float)deltaTime * reverse);
 
             if (Raylib.IsKeyDown(KeyboardKey.A))
-                Transform.Translate(Transform.Right * (float)deltaTime);
+                Transform.Rotate(RotateSpeed * (float)deltaTime * reverse);
            
             if (Raylib.IsKeyDown(KeyboardKey.D))
-                Transform.Translate(Transform.Right * (float)deltaTime * reverse);
+                Transform.Rotate(RotateSpeed * (float)deltaTime);
 
         }
     }
