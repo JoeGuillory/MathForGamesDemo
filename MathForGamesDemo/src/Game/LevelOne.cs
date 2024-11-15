2e110e112d14d84c57b5e79f046af0483adc9308
JoeGuillory <joeguillory15@gmail.com>
2024-11-14 19:12:41 -0600

    Update LevelOne.cs

--- MathForGamesDemo/src/Game/LevelOne.cs
+++ MathForGamesDemo/src/Game/LevelOne.cs
@@ -17,7 +17,7 @@ namespace MathForGamesDemo
 
             Actor background = Actor.Instantiate(new Tile(), null);
             Actor playerTank = Actor.Instantiate(new PlayerTank(), null, new Vector2(Raylib.GetScreenWidth()/2,Raylib.GetScreenHeight()/2));
-            Actor followTank = Actor.Instantiate(new FollowTank(), playerTank.Transform);
+            
 
             Actor enemyTank = Actor.Instantiate(new EnemyTank());
             enemyTank.AddComponent<LookAt>(new LookAt(enemyTank, playerTank));
